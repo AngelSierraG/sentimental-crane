5a04df7ef141d21064a85e2a8d45091fa331318d
flemon.ghobrial@gmail.com <flemon.ghobrial@gmail.com>
2012-11-22 18:40:38 +0100

    Scheduler & Manager alpha

--- Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/ClusterManager.java
+++ Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/ClusterManager.java
@@ -2,7 +2,28 @@ package at.ac.tuwien.aic.sc.scheduler;
 
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
+
+import org.openstack.keystone.KeystoneClient;
+import org.openstack.keystone.api.Authenticate;
+import org.openstack.keystone.api.ListTenants;
+import org.openstack.keystone.model.Access;
+import org.openstack.keystone.model.Authentication;
+import org.openstack.keystone.model.Tenants;
+import org.openstack.keystone.model.Authentication.PasswordCredentials;
+import org.openstack.keystone.model.Authentication.Token;
+import org.openstack.keystone.utils.KeystoneUtils;
+import org.openstack.nova.NovaClient;
+import org.openstack.nova.api.FlavorsCore;
+import org.openstack.nova.api.ServersCore;
+import org.openstack.nova.model.Flavor;
+import org.openstack.nova.model.Flavors;
+import org.openstack.nova.model.Server;
+import org.openstack.nova.model.ServerForCreate;
 
 /**
  * @author Dominik Strasser, dominikstr@gmail.com
@@ -11,17 +32,98 @@ import java.util.logging.Logger;
 public class ClusterManager {
 	private static final Logger logger = Logger.getLogger(ClusterManager.class.getName());
 
+	private static final String KEYSTONE_AUTH_URL = "http://openstack.infosys.tuwien.ac.at:5000/v2.0";
+	private static final String KEYSTONE_USERNAME = "aic12w02";
+	private static final String KEYSTONE_PASSWORD = "Vu6Sotee";
+	
+	private KeystoneClient keystone;
+	private Authentication authentication;
+	private PasswordCredentials passwordCredentials;
+	private Access access;
+	private Tenants tenants;
+	private NovaClient novaClient;
+	
+	private int nodeCounter;
+	private Map<String, Server> servers;
+	
+	public ClusterManager(){
+		keystone = new KeystoneClient(KEYSTONE_AUTH_URL);
+		authentication = new Authentication();
+		passwordCredentials = new PasswordCredentials();
+		passwordCredentials.setUsername(KEYSTONE_USERNAME);
+		passwordCredentials.setPassword(KEYSTONE_PASSWORD);
+		authentication.setPasswordCredentials(passwordCredentials);
+
+		nodeCounter=0;
+		servers = new HashMap<String, Server>();
+		
+		//access with unscoped token
+		access = keystone.execute(new Authenticate(authentication));
+
+		//use the token in the following requests
+		keystone.setToken(access.getToken().getId());
+
+		tenants = keystone.execute(new ListTenants());
+		//try to exchange token using the first tenant
+		if(tenants.getList().size() > 0) {
+
+			authentication = new Authentication();
+			Token token = new Token();
+			token.setId(access.getToken().getId());
+			authentication.setToken(token);
+			authentication.setTenantId(tenants.getList().get(0).getId());
+
+			access = keystone.execute(new Authenticate(authentication));
+
+			novaClient = new NovaClient(KeystoneUtils.findEndpointURL(access.getServiceCatalog(), "compute", null, "public"), access.getToken().getId());
+
+			Flavors flavors = novaClient.execute(FlavorsCore.listFlavors());
+			for(Flavor flavor : flavors) {
+				System.out.println(flavor);
+			}
+
+		} else {
+			System.out.println("No tenants found!");
+		}
+	}
+	
 	@Asynchronous
 	public void startClusterNode() {
+		ServerForCreate serverForCreate = new ServerForCreate();
+		serverForCreate.setName("node" + nodeCounter);
+		serverForCreate.setFlavorRef("100"); 	// flavor = type of machine -> find out id of m1.tiny
+		serverForCreate.setImageRef("120"); 	// reference of image or snapshot -> find out id of snapshot
+		serverForCreate.setKeyName("node" + nodeCounter);
+		serverForCreate.getSecurityGroups().add(new ServerForCreate.SecurityGroup("default"));
+		
+		Server server = novaClient.execute(ServersCore.createServer(serverForCreate));
+		
+		servers.put(server.getId(), server);
+		
+		System.out.println("Created: \n" + server);
+		
 		logger.info("Starting up cluster node");
+
 	}
 
 	@Asynchronous
-	public void shutdownClusterNode() {
+	public void shutdownClusterNode(String id) {
+		novaClient.execute(ServersCore.deleteServer(id));
+		servers.remove(id);
 		logger.info("shutting down cluster node");
+	}
+	
+	@Asynchronous
+	public void shutdownAllClusterNodes() {
+		for(Entry<String, Server> server : servers.entrySet()) {
+			novaClient.execute(ServersCore.deleteServer(server.getKey()));
+			servers.remove(server.getKey());
+		}
+
+		logger.info("shutting down all cluster nodes");
 	}
 
 	public int getNumberOfRunningNodes() {
-		return 0;
+		return servers.size();
 	}
 }
--- Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/OpenstackScheduler.java
+++ Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/OpenstackScheduler.java
@@ -7,6 +7,9 @@ import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Observes;
+
+
+
 import java.util.logging.Logger;
 
 /**
@@ -17,9 +20,18 @@ public class OpenstackScheduler {
 	@EJB
 	private ClusterManager clusterManager;
 
+	private final int MaxNumberOfClusterNodes = 7;
+	
 	private static final Logger logger = Logger.getLogger(OpenstackScheduler.class.getName());
 
+	public OpenstackScheduler(){
+
+	}
+	
 	public void newAnalysis(@Observes AnalysisStartEvent event) {
+		for(int i = 0; i<=MaxNumberOfClusterNodes; i++){
+			clusterManager.startClusterNode();
+		}
 		logger.info("New analysis: " + event.getCompanyName() + " (" + event.getFrom() + " - " + event.getTo() + ")");
 	}
 
