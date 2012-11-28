9b12197ebce6f84150f29b2e0370b182bfe7dd88
Gregor Schauer <e0926086@student.tuwien.ac.at>
2012-11-28 17:49:32 +0100

    changed from suspend/resume to terminate/start

--- Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/ClusterManager.java
+++ Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/ClusterManager.java
@@ -1,10 +1,7 @@
 package at.ac.tuwien.aic.sc.scheduler;
 
 import at.ac.tuwien.aic.sc.core.event.ServerInstanceChangeEvent;
-import at.ac.tuwien.aic.sc.scheduler.json.Access;
-import at.ac.tuwien.aic.sc.scheduler.json.Credentials;
-import at.ac.tuwien.aic.sc.scheduler.json.Server;
-import at.ac.tuwien.aic.sc.scheduler.json.Servers;
+import at.ac.tuwien.aic.sc.scheduler.json.*;
 import com.sun.jersey.api.client.*;
 import com.sun.jersey.api.client.filter.ClientFilter;
 
@@ -14,10 +11,12 @@ import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML_TYPE;
 
 /**
  * @author Dominik Strasser, dominikstr@gmail.com
@@ -29,15 +28,59 @@ public class ClusterManager {
 	public static final String TENANT_ID = "cfa61aed5c0a4d6bb10d111e63a3337c";
 	public static final String USERNAME = "aic12w02";
 	public static final String PASSWORD = "Vu6Sotee";
+	private static int ID = new Random().nextInt(9990) + 10;
 
 	@Inject
 	private Event<ServerInstanceChangeEvent> changeEvent;
+
+	public static void main(String[] args) {
+		ClusterManager cm = new ClusterManager();
+		cm.startClusterNodes(1);
+	}
+
+	private void listImages() {
+		Client client = getClient();
+		String url = getUrl("2", TENANT_ID, "images", "detail");
+		String response = client.resource(url)
+				.type(APPLICATION_JSON_TYPE).accept(APPLICATION_XML_TYPE).get(String.class);
+		System.out.println(response);
+	}
+
+	private void listFlavors() {
+		Client client = getClient();
+		String url = getUrl("2", TENANT_ID, "flavors", "detail");
+		String response = client.resource(url)
+				.type(APPLICATION_JSON_TYPE).accept(APPLICATION_XML_TYPE).get(String.class);
+		System.out.println(response);
+	}
 
 	@Asynchronous
 	public void startClusterNodes(final int n) {
 		if (n <= 0) {
 			return;
 		}
+
+		ServerCreate serverCreate = new ServerCreate();
+		serverCreate.server.name = "node" + (ID++);
+		serverCreate.server.flavorRef = "1";
+		serverCreate.server.imageRef = "http://openstack.infosys.tuwien.ac.at/"+TENANT_ID+"/images/fc5f3db2-6430-4875-8955-2a86eb1a8699";
+
+		String request = "{\n" +
+				"    \"server\": {\n" +
+				"        \"flavorRef\": \""+serverCreate.server.flavorRef+"\",\n" +
+				"        \"imageRef\": \""+serverCreate.server.imageRef+"\",\n" +
+				"        \"name\": \"node03\"" +
+				"    }\n" +
+				"}";
+
+		Client client = getClient();
+		client.resource(getUrl("2", TENANT_ID, "servers"))
+				.type(APPLICATION_JSON_TYPE).post(request);
+		client.destroy();
+	}
+
+	@Deprecated
+	private void resume(final int n) {
 		int cnt = 0;
 		List<Server> servers = getServers();
 		for (Server server : servers) {
@@ -81,7 +124,7 @@ public class ClusterManager {
 		for (Server server : servers) {
 			if (server.name.startsWith("node") && !server.name.equals("node01") && server.isActive()) {
 				try {
-					action(server, "suspend");
+					terminate(server);
 
 					if (!server.isActive() && logger.isLoggable(Level.INFO)) {
 						logger.info("Last known status of server " + server.name + " was " + server.status);
@@ -95,6 +138,13 @@ public class ClusterManager {
 				}
 			}
 		}
+	}
+
+	private void terminate(Server server) {
+		Client client = getClient();
+		client.resource(getUrl("2", TENANT_ID, "servers", server.id, "action"))
+				.type(APPLICATION_JSON_TYPE).delete();
+		client.destroy();
 	}
 
 	public int getNumberOfRunningNodes() {
