1d890dbdb3cf39cb6feb0b8476b27152659c6dfc
Dominik Strasser <dominikstr@gmail.com>
2012-11-28 09:21:50 +0100

    changed scheduler

--- Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/OpenStackScheduler.java
+++ Sentimental-Crane/Sentimental-Crane-Scheduler/src/main/java/at/ac/tuwien/aic/sc/scheduler/OpenStackScheduler.java
@@ -2,18 +2,12 @@ package at.ac.tuwien.aic.sc.scheduler;
 
 import at.ac.tuwien.aic.sc.core.event.AnalysisEndEvent;
 import at.ac.tuwien.aic.sc.core.event.AnalysisStartEvent;
-import at.ac.tuwien.aic.sc.scheduler.support.ComputationLoadPredictor;
-import at.ac.tuwien.aic.sc.scheduler.support.SimpleComputationLoadPredictor;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.enterprise.event.Observes;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -23,34 +17,33 @@ import java.util.logging.Logger;
 @Singleton
 public class OpenStackScheduler {
 	static final Logger logger = Logger.getLogger(OpenStackScheduler.class.getName());
-	ComputationLoadPredictor loadPredictor = new SimpleComputationLoadPredictor();
 	@EJB
 	ClusterManager clusterManager;
+
+	Date lastAnalysisDate = null;
+
+	int idletime = 20;
 
 	public OpenStackScheduler() {
 	}
 
 	public void newAnalysis(@Observes AnalysisStartEvent event) {
 		logger.info("New analysis: " + event.getCompanyName() + " (" + event.getFrom() + " - " + event.getTo() + ")");
+		lastAnalysisDate = new Date();
 
 		int currentNodeCount = clusterManager.getNumberOfRunningNodes();
-		int requiredNodeCount = loadPredictor.predict(event);
 
-		logger.info("Currently " + currentNodeCount + " nodes are available - " + requiredNodeCount + " nodes are required.");
-		if (requiredNodeCount > currentNodeCount) {
-			clusterManager.startClusterNodes(requiredNodeCount - currentNodeCount);
-		}
+		clusterManager.startClusterNodes(8 - currentNodeCount);
 	}
 
 	public void analysisEnded(@Observes AnalysisEndEvent event) {
 		logger.info("Analysis ended");
+	}
 
-		int currentNodeCount = clusterManager.getNumberOfRunningNodes();
-		int requiredNodeCount = loadPredictor.predict(event);
-
-		logger.info("Currently " + currentNodeCount + " nodes are available - " + requiredNodeCount + " nodes are required.");
-		if (currentNodeCount > requiredNodeCount) {
-			clusterManager.shutdownClusterNodes(currentNodeCount - requiredNodeCount);
+	@Schedule(second = "*/20", minute = "*", hour = "*", persistent = false)
+	public void run() {
+		if (lastAnalysisDate != null && (TimeUnit.MILLISECONDS.toSeconds(new Date().getTime() - lastAnalysisDate.getTime()) > idletime)) {
+			clusterManager.shutdownClusterNodes(1);
 		}
 	}
 }
--- Sentimental-Crane/Sentimental-Crane-Scheduler/src/test/java/at/ac/tuwien/aic/sc/scheduler/OpenStackSchedulerTest.java
+++ Sentimental-Crane/Sentimental-Crane-Scheduler/src/test/java/at/ac/tuwien/aic/sc/scheduler/OpenStackSchedulerTest.java
@@ -1,9 +1,6 @@
 package at.ac.tuwien.aic.sc.scheduler;
 
-import at.ac.tuwien.aic.sc.core.event.AnalysisEndEvent;
 import at.ac.tuwien.aic.sc.core.event.AnalysisStartEvent;
-import at.ac.tuwien.aic.sc.scheduler.support.SimpleComputationLoadPredictor;
-import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,101 +15,41 @@ public class OpenStackSchedulerTest {
 	private Date dataBeginDate;
 	private Date dataEndDate;
 	private OpenStackScheduler scheduler;
-	SimpleComputationLoadPredictor loadPredictor;
 
 	@Before
 	public void before() throws Exception {
-		scheduler  = new OpenStackScheduler();
+		scheduler = new OpenStackScheduler();
 		scheduler.clusterManager = new ClusterManagerMock();
 
-		loadPredictor = new SimpleComputationLoadPredictor();
-		scheduler.loadPredictor = loadPredictor;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		dataEndDate = format.parse("2011-08-02");
+		dataBeginDate = format.parse("2011-07-29");
+	}
 
-		dataBeginDate = (Date) FieldUtils.readField(loadPredictor, "dataBeginDate", true);
-		dataEndDate = (Date) FieldUtils.readField(loadPredictor, "dataEndDate", true);
+
+	@Test
+	public void testNewAnalysis() {
+		scheduler.newAnalysis(new AnalysisStartEvent("Google", DateUtils.addDays(dataBeginDate, -1), DateUtils.addDays(dataBeginDate, +1)));
+		assertEquals(8, scheduler.clusterManager.getNumberOfRunningNodes());
 	}
 
 	@Test
-	public void testOneDay() {
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(0, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		AnalysisStartEvent startEvent = new AnalysisStartEvent("Google", new Date(0), new Date(0));
-		scheduler.newAnalysis(startEvent);
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		scheduler.analysisEnded(new AnalysisEndEvent(startEvent.getEventId()));
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
+	public void testShutdownSchedule() {
+		scheduler.clusterManager.startClusterNodes(8);
+		scheduler.lastAnalysisDate = new Date();
+		scheduler.idletime = -1;
+		scheduler.run();
+		assertEquals(7, scheduler.clusterManager.getNumberOfRunningNodes());
 	}
 
 	@Test
-	public void testHundredDays() {
-		Date start = new Date(0);
-		Date end = DateUtils.addDays(start, 50);
-
-		AnalysisStartEvent startEvent = new AnalysisStartEvent("Google", start, end);
-		scheduler.newAnalysis(startEvent);
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		scheduler.analysisEnded(new AnalysisEndEvent(startEvent.getEventId()));
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		end = DateUtils.addDays(start, 100);
-
-		startEvent = new AnalysisStartEvent("Google", start, end);
-		scheduler.newAnalysis(startEvent);
-		assertEquals(2, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(2, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		scheduler.analysisEnded(new AnalysisEndEvent(startEvent.getEventId()));
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
-	}
-
-	@Test
-	public void testAnalyseSpecialBegin() {
-		Date start = DateUtils.addDays(dataBeginDate, -1);
-		Date end = DateUtils.addDays(dataBeginDate, +1);
-
-		analyseSpecial(start, end);
-	}
-
-	@Test
-	public void testAnalyseSpecialEnd() {
-		Date start = DateUtils.addDays(dataEndDate, -1);
-		Date end = DateUtils.addDays(dataEndDate, +1);
-
-		analyseSpecial(start, end);
-	}
-
-	@Test
-	public void testAnalyseSpecialBetween() {
-		Date start = DateUtils.addDays(dataBeginDate, +1);
-		Date end = DateUtils.addDays(dataEndDate, -1);
-
-		analyseSpecial(start, end);
-	}
-
-	@Test
-	public void testAnalyseSpecialAll() {
-		Date start = DateUtils.addDays(dataBeginDate, -1);
-		Date end = DateUtils.addDays(dataEndDate, +1);
-
-		analyseSpecial(start, end);
-	}
-
-	private void analyseSpecial(Date start, Date end) {
-		AnalysisStartEvent startEvent = new AnalysisStartEvent("Google", start, end);
-		scheduler.newAnalysis(startEvent);
-		assertEquals(loadPredictor.maxNumberOfClusterNodes, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(loadPredictor.maxNumberOfClusterNodes, scheduler.clusterManager.getNumberOfRunningNodes());
-
-		scheduler.analysisEnded(new AnalysisEndEvent(startEvent.getEventId()));
-		assertEquals(1, loadPredictor.calculateRequiredNodeCount());
-		assertEquals(1, scheduler.clusterManager.getNumberOfRunningNodes());
+	public void testMultipleShutdown(){
+		scheduler.clusterManager.startClusterNodes(8);
+		scheduler.lastAnalysisDate = new Date();
+		scheduler.idletime = -1;
+		scheduler.run();
+		scheduler.run();
+		scheduler.run();
+		assertEquals(5, scheduler.clusterManager.getNumberOfRunningNodes());
 	}
 }
